b37150fdf06de1995ffa423479cf90f3b106cf8d
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2024-10-02 18:03:59 -0400

    Bug fix with pausing

--- Scripts/UI/pause_menu.gd
+++ Scripts/UI/pause_menu.gd
@@ -1,18 +1,23 @@
 extends Control
 
-func _ready() -> void:
-	$AnimationPlayer.play("RESET")
+@onready var world = get_parent().get_parent()
+
+#func _ready() -> void:
+	#$AnimationPlayer.play("RESET")
 	
 func pause() -> void:
+	print("Paused")
 	get_tree().paused = true
 	show()
 	$AnimationPlayer.play("Blur")
 	
 func resume():
+	print("Resuming Game...")
 	$AnimationPlayer.play_backwards("Blur")
 	await $AnimationPlayer.animation_finished
 	hide()
 	get_tree().paused = false
+	world.paused = false
 	
 
 func _on_resume_button_pressed() -> void:
