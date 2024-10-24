276c38e511690f9d811298c290ce422707a40adc
Julio <88823380+JULLACE@users.noreply.github.com>
2024-10-24 18:18:24 -0400

    Background changes, added more to 2nd level tilemap, fixed lava

--- Scenes/Levels/tile_detector.gd
+++ Scenes/Levels/tile_detector.gd
@@ -28,30 +28,31 @@ func _ready():
 	# Get all used cells in the entire TileMap
 	var used_cells = get_used_cells(0)  # Assuming layer 0, iterate all cells
 	
+	var lava_cells: Array[Vector2i]
 	# Iterate through every tile in the used cells to manually check the ID
 	for cell_pos in used_cells:
 		var tile_data = get_cell_tile_data(0, cell_pos)  # Using layer 0 for now
 		var tile_id = tile_data.get_custom_data_by_layer_id(TERRAIN_ID_LAYER)
 		
-		# Check if the tile ID matches the custom tile data ID
+		 #Check if the tile ID matches the custom tile data ID
 		if tile_id == TerrainType.LAVA:
+			lava_cells.push_back(cell_pos)
 			
-			# Calculate the world position manually
-			var world_pos = Vector2(cell_pos.x * cell_size.x, cell_pos.y * cell_size.y)
+	lava_cells.sort()
+	
+	for n in range(0, lava_cells.size(), 32):
+		var cell_pos = lava_cells[n]
+		var light_instance = PointLight2D.new()
 
-			# Create a new PointLight2D node
-			var light_instance = PointLight2D.new()
+		# Attach the texture to the light
+		light_instance.texture = light_texture
+		light_instance.position = map_to_local(cell_pos) # Center the light on the tile
 
-			# Attach the texture to the light
-			light_instance.texture = light_texture
-			light_instance.position = world_pos + cell_size / 2.0  # Center the light on the tile
+		# Adjust brightness and color for a lava-like effect
+		light_instance.energy = 2  # Reduce brightness, try values between 0.1 and 0.5
+		light_instance.color = Color8(255, 69, 0, 255)  # Set to a reddish-orange color (RGB: 255, 69, 0)
 
-			# Adjust brightness and color for a lava-like effect
-			light_instance.energy = 0.4  # Reduce brightness, try values between 0.1 and 0.5
-			light_instance.color = Color8(255, 69, 0, 255)  # Set to a reddish-orange color (RGB: 255, 69, 0)
-
-			# Adjust light properties
-			light_instance.z_index = 10  # Draw above other elements
-
-			# Add the light to the TileMap
-			add_child(light_instance)
+		# Adjust light properties
+		light_instance.blend_mode = Light2D.BLEND_MODE_ADD
+		
+		add_child(light_instance)
