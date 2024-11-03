816fc751b686299db2564ddda1dc6717cfdd6329
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2024-11-02 22:01:34 -0400

    Added interactionlabel func to Storage

--- Scenes/Ship/storage_area_2d.gd
+++ Scenes/Ship/storage_area_2d.gd
@@ -3,18 +3,23 @@ extends Area2D
 @export var interact_key: String = "interact"  # represents the "E" key by default
 var player_nearby: bool = false
 @onready var ship_storage_ui: Control = $"../../CanvasLayer/ShipStorageUI"
+@onready var player: Player = get_node("/root/Shipinterior/Player")
+@onready var interaction_label = player.get_node("InteractionLabel")
+
 
 # Detect when the player enters the navigation computer's area
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):  # Ensure the player is in the player group
 		player_nearby = true
 		print("Player is near the storage locker. Press E to interact.")
+		show_interaction_message("Press E to interact")
 
 # Detect when the player leaves the area
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player_nearby = false
 		print("Player has left the storage locker area.")
+		hide_interaction_message()
 
 # Check for the interaction key press
 func _process(delta: float) -> void:
@@ -24,3 +29,12 @@ func _process(delta: float) -> void:
 		else:
 			ship_storage_ui.open(Globals.inventory, Globals.ship_inventory)
 		print("Storage menu toggled.")
+
+# Show the interaction message over the player's head
+func show_interaction_message(message: String):
+	interaction_label.text = message
+	interaction_label.visible = true
+
+# Hide the interaction message
+func hide_interaction_message():
+	interaction_label.visible = false
