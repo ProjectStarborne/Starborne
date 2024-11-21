a8f60ed2d43cc15879f07525676c63c6faff7944
Markusraym <ploskiray@gmail.com>
2024-11-21 13:58:33 -0500

    fixed upgrade prices, removed tape

--- Scripts/UI/Shop/item_upgrades.gd
+++ Scripts/UI/Shop/item_upgrades.gd
@@ -42,7 +42,6 @@ func get_shop_items_for_level() -> Array[Item]:
 	var items_for_level: Array[Item] = []
 	items_for_level.append(load("res://Data/Items/Tools/drill.tres") as Item)
 	items_for_level.append(load("res://Data/Items/Tools/flashlight.tres") as Item)
-	items_for_level.append(load("res://Data/Items/Consumables/duct_tape.tres") as Item)
 	items_for_level.append(load("res://Data/Items/Consumables/medkit.tres") as Item)
 	items_for_level.append(load("res://Data/Items/Consumables/oxygen_tank.tres") as Item)
 
