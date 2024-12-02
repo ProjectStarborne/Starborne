c7ec33db52ed4453ed76b78109de298112febb7b
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2024-12-02 17:37:13 -0500

    Added notifications signal to broken logs

--- Scripts/Logs/broken_log.gd
+++ Scripts/Logs/broken_log.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var detector: Area2D = $Area2D
 
 signal log_pickup(num)
+signal notification(msg)
 
 func _ready() -> void:
 	animation_player.play("flashing")
@@ -12,4 +13,5 @@ func _ready() -> void:
 
 func on_pickup(body: Node2D) -> void:
 	log_pickup.emit(log_number)
+	notification.emit("Picked up Log #" + str(log_number) + "!")
 	queue_free()
