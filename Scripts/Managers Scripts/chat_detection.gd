3c7ce53b7460541cb2ddab180f2f966a9bf3366f
Markusraym <ploskiray@gmail.com>
2024-12-04 19:13:19 -0500

    Added dialogue to doctor and engineer. Fixed the 20000 credits

--- Scripts/Managers Scripts/chat_detection.gd
+++ Scripts/Managers Scripts/chat_detection.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var player_in_area = false
 @export var interact_key: String = "interact"  # Represents the "E" key by default
+@export var dialogue_name: String = ""  # Name of the dialogue timeline (e.g., "overseer_timeline" or "engineer_timeline")
 
 @onready var player = get_node("/root/Environment2/Player")  # Update this path to your actual scene structure
 @onready var interaction_label = player.get_node("InteractionLabel")  # Label above the player's head
@@ -11,7 +12,8 @@ func _process(delta: float) -> void:
 	if player_in_area:
 		if Input.is_action_just_pressed(interact_key):
 			# Run Dialogue
-			run_dialogue("overseer_timeline")
+			if dialogue_name != "":
+				run_dialogue(dialogue_name)
 
 # Function to run dialogue
 func run_dialogue(name: String):
@@ -21,14 +23,14 @@ func run_dialogue(name: String):
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player_in_area = true
-		print("Player is near overseer. Press E to interact.")
+		print("Player is near an NPC. Press E to interact.")
 		show_interaction_message("Press E to interact")
 
 # Detect when the player leaves the interaction area
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player_in_area = false
-		print("Player has left the overseer area.")
+		print("Player has left the NPC area.")
 		hide_interaction_message()
 
 # Show the interaction message over the player's head
